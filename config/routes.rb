e6a602a62a8d86bd6af7c00514f9fd1a944e0c84
prakash thapa <pt6769759@gmail.com>
2021-04-01 18:47:55 +0545

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "posts#index"
+  get "/posts", to: "posts#index"
+  get "/posts/:id", to: "posts#show"
+
+#api endpoints
+  get "/api/todos", to: "posts#api" 
+    
+
 end
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TodoData from './com/data';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Todos</h1>
+       <TodoData/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
